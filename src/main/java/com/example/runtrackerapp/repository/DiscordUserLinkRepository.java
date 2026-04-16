af06f2e3dc279afc99e3c9d8849e55440152b4bc
Sam <mbaselasam@gmail.com>
2026-04-16 10:38:44 -0600

    Add Discord bot integration and user linking functionality

--- src/main/java/com/example/runtrackerapp/repository/DiscordUserLinkRepository.java
+++ src/main/java/com/example/runtrackerapp/repository/DiscordUserLinkRepository.java
@@ -1,4 +1,9 @@
 package com.example.runtrackerapp.repository;
 
-public class DiscordUserLinkRepository {
+import com.example.runtrackerapp.model.DiscordUserLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface DiscordUserLinkRepository extends JpaRepository<DiscordUserLink, Long> {
+    Optional<DiscordUserLink> findByDiscordUserId(String discordUserId);
 }
